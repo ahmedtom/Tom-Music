c46506638af17a07efbfabfa51fa8b178524b7c5
ahmedtom <101574426+ahmedtom@users.noreply.github.com>
2022-06-22 12:58:25 +0200

    Update start.py

--- program/start.py
+++ program/start.py
@@ -83,9 +83,9 @@ async def _human_time_duration(seconds):
 async def start_(c: Client, message: Message):
     user_id = message.from_user.id
     await add_served_user(user_id)
-    await message.reply_text()
-    await message.reply_photo(
+await message.reply_photo(
         photo=f"https://telegra.ph//file/cdcf6e35335e7a01f0948.jpg",
+    await message.reply_text(
         f"""ههݪاެ يا حب {message.from_user.mention()} ❤️‍🔥\n
  انا بوت لتشغيل الاغاني في المجموعات اعمل على سورس توم بمميزات عديدة
 ┏━━━━━━━━━━━━━━┓
